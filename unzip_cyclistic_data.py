ec8a1b0da1989021d2251748bcda11cd0e617f09
Angelo-Bobotas <59439456+Angelo-Bobotas@users.noreply.github.com>
2022-10-14 11:51:06 +0300

    Update unzip_cyclistic_data.py

--- unzip_cyclistic_data.py
+++ unzip_cyclistic_data.py
@@ -3,9 +3,9 @@ import os
 
 
 zips = os.listdir('/Data')
-for zipNames in zips:
+for zipName in zips:
     with open(os.path.join('/Data', 'cyclisticData.7z'), 'ab') as f:
-        with open(os.path.join('/Data'), 'rb') as z:
+        with open(os.path.join('/Data', zipName), 'rb') as z:
                   f.write(z.read())
                   
 with py7zr.SevenZipFile(os.path.join('/Data', 'cyclisticData.7z'), "r") as archive:
